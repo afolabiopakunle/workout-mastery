82b6f9208d737a9a0055703345315b903d4435cd
Afolabi <opakunleafolabi@gmail.com>
2022-10-05 18:04:19 +0100

    router implemented

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import {Box} from '@mui/material';
 import Navbar from "./compoonents/Navbar";
+import Footer from "./compoonents/Footer";
+
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
 
 
 function App(props) {
     return (
-        <Box width='400px'>
+        <Box width='400px' sx={{ width: {xl: '1488px'} }} m='auto'>
            <Navbar />
            <Routes>
                <Route path='/' element={<Home />} />
-               <Routes path='/exercise/:id' element={<ExerciseDetail />} />
+               <Route path='/exercise/:id' element={<ExerciseDetail />} />
            </Routes>
+           <Footer />
         </Box>
     );
 }
---
+++ src/pages/ExerciseDetail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ExerciseDetail(props) {
+    return (
+        <div>Exercise Detail</div>
+    );
+}
+
+export default ExerciseDetail;
