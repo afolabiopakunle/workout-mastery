f91f9042561815a683fe04b89e89b3aa88b4189f
Afolabi <opakunleafolabi@gmail.com>
2022-10-05 17:45:44 +0100

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import {Box} from '@mui/material';
+import Navbar from "./compoonents/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(props) {
+    return (
+        <Box width='400px'>
+           <Navbar />
+           <Routes>
+               <Route path='/' element={<Home />} />
+               <Routes path='/exercise/:id' element={<ExerciseDetail />} />
+           </Routes>
+        </Box>
+    );
 }
 
 export default App;
